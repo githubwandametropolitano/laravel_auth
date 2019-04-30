4c19f19bf362b6ca108b800fd6bfe36ca6f01719
githubwandametropolitano <ls4694471@gmail.com>
2019-04-30 03:46:25 +0300

    removesearch

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -109,20 +109,9 @@
 			</ul>
 		</nav> <!-- end main-nav-wrap -->
 
-		<div class="search-wrap">
-			<form role="search" method="get" class="search-form" action="{{ route('posts.search') }}">
-				<label>
-					<input type="search" class="search-field" placeholder="@lang('Type Your Keywords')"  name="search" autocomplete="off" required>
-				</label>
-				<input type="submit" class="search-submit" value="">
-			</form>
 
-			<a href="#" id="close-search" class="close-btn">Close</a>
-
-		</div> <!-- end search wrap -->
 
 		<div class="triggers">
-			<a class="search-trigger" href="#"><i class="fa fa-search"></i></a>
 			<a class="menu-toggle" href="#"><span>Menu</span></a>
 		</div> <!-- end triggers -->
 
@@ -134,69 +123,6 @@
 
    <!-- footer
    ================================================== -->
-   <footer>
-
-   	<div class="footer-main">
-
-   		<div class="row">
-
-	      	<div class="col-six tab-full mob-full footer-info">
-
-	            <h4>@lang('About Our Site')</h4>
-
-	               <p>@lang('Lorem ipsum Ut velit dolor Ut labore id fugiat in ut fugiat nostrud qui in dolore commodo eu magna Duis cillum dolor officia esse mollit proident Excepteur exercitation nulla. Lorem ipsum In reprehenderit commodo aliqua irure labore.')</p>
-
-		      </div> <!-- end footer-info -->
-
-	      	<div class="col-three tab-1-2 mob-1-2 site-links">
-
-	      		<h4>@lang('Site Links')</h4>
-
-	      		<ul>
-				  	<li><a href="#">@lang('About us')</a></li>
-					<li><a href="{{ url('') }}">@lang('Blog')</a></li>
-					<li><a href="{{ route('contacts.create') }}">@lang('Contact')</a></li>
-					<li><a href="#">@lang('Privacy Policy')</a></li>
-				</ul>
-
-	      	</div> <!-- end site-links -->
-
-	      	<div class="col-three tab-1-2 mob-1-2 social-links">
-
-	      		<h4>@lang('Social')</h4>
-
-	      		<ul>
-	      			<li><a href="#">Twitter</a></li>
-					<li><a href="#">Facebook</a></li>
-					<li><a href="#">Dribbble</a></li>
-					<li><a href="#">Google+</a></li>
-					<li><a href="#">Instagram</a></li>
-				</ul>
-
-	      	</div> <!-- end social links -->
-
-	      </div> <!-- end row -->
-
-   	</div> <!-- end footer-main -->
-
-      <div class="footer-bottom">
-      	<div class="row">
-
-      		<div class="col-twelve">
-	      		<div class="copyright">
-		         	<span>© Copyright Abstract 2016</span>
-		         	<span>Design by <a href="http://www.styleshout.com/">styleshout</a></span>
-		         </div>
-
-		         <div id="go-top">
-		            <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon icon-arrow-up"></i></a>
-		         </div>
-	      	</div>
-
-      	</div>
-      </div> <!-- end footer-bottom -->
-
-   </footer>
 
    <div id="preloader">
     	<div id="loader"></div>
