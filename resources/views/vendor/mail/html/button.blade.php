9df6255e550f62de66d5553ef820d1f798c0235a
githubwandametropolitano <ls4694471@gmail.com>
2019-05-04 17:52:59 +0300

    fly

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -6,8 +6,8 @@
                     <td align="center">
                         <table border="0" cellpadding="0" cellspacing="0">
                             <tr>
-                                <td style="background-color: rgb(127, 9, 100); height: 3em; border-radius: 7px;">
-                                    <a href="{{ $url }}" class="button" target="_blank">{{ $slot }}</a>
+                                <td >
+                                    <a href="{{ $url }}" class="button" target="_blank" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;border-radius:7px;color:#fff;display:inline-block;text-decoration:none;background-color: rgb(127, 9, 100);height: 3em;text-align: center;border-top: 10px solid rgb(127,9, 100);border-right: 18px solid rgb(127,9, 100);border-bottom: 10px solid rgb(127,9, 100);border-left: 18px solid rgb(127,9,100);">{{ $slot }}</a>
                                 </td>
                             </tr>
                         </table>
